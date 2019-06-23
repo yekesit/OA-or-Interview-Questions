1be2fd87601702045495dea09d8f7f5502fa3d66
Ke Ye <yekesit@gmail.com>
2019-06-23 14:09:20 -0700

    amazon round robin. doesn't finish

--- amazon/round_robin.cpp
+++ amazon/round_robin.cpp
@@ -3,9 +3,28 @@
 //
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+double roundRobin(vector<int>& arrive, vector<int>& excute, int q){
+    if(arrive.empty() || excute.empty()) return 0.0;
+    vector<pair<int, int>> tasks(arrive.size());
+    double sum = 0;
+    for(int i = 0; i < arrive.size(); i++){
+        tasks[i].first = arrive[i];
+        tasks[i].second = excute[i];
+    }
+    for(auto& t : tasks){
+        cout << t. first << ' ' << t.second << endl;
+    }
+    return sum;
+}
+
+
 int main(){
-    cout << 1 << endl;
+    vector<int> arrive{0,1,3,9};
+    vector<int> excute{2,1,7,5};
+    int q = 3;
+    cout << roundRobin(arrive, excute, q);
 }
